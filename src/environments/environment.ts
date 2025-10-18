448d2f58e8ce653ba8af1f17f6e6ed49a8ff8bf1
Ayodeji Adebola <ayodejiadebola20@gmail.com>
2025-10-18 01:32:27 +0100

    tested form

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  emailjsServiceId: 'service_ji0yk6i',
+  emailjsServiceId: 'service_erbp603',
   emailjsTemplateId: 'template_prvi7rc',
   emailjsPublicKey: 'bdGXw8SG9ew2pZgHx'
 };
